4c85f84016cbcedf9a73148e8ccd193128bcbe11
ahmedmorsy97 <ahmedsamirmorsi@hotmail.com>
2018-02-20 22:34:20 +0200

    edited part of rigester form

--- src/app/dashboard/register/register.component.ts
+++ src/app/dashboard/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl,FormBuilder, FormGroup,Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+    regForm: FormGroup;
+    register:any;
+    username:string = '';
+    password:string = '';
+    cpassword:string = '';
+    user_req:string = 'This filed is required';
+    pass_req:string = 'This filed is required';
+    same:string = '';
+    passdm:string = 'Password does not match';
+  constructor(private fb: FormBuilder) {
+    this.regForm = fb.group({
+        'username' : [null, Validators.required],
+        'password' : [null, Validators.required],
+           // Validators.pattern(/^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{4,20}/),
+        'cpassword' : [null, Validators.required],
+        validator: this.passwordConfirming
+    });
 
-  constructor() { }
+  }
 
   ngOnInit() {
-  }
+
+}
+
+    passwordConfirming(c: AbstractControl): { invalid: boolean } {
+        if (c.get('password').value !== c.get('cpassword').value) {
+            return {invalid: true};
+        }
+    }
+  userinfo(register){
+      this.username=register.username;
+      this.password=register.password;
+      this.cpassword=register.cpassword;
+}
+
 
 }
--- src/app/dashboard/register/register.module.ts
+++ src/app/dashboard/register/register.module.ts
@@ -4,10 +4,16 @@ import { ThemeModule } from '../../@theme/theme.module';
 
 import { RegisterComponent } from './register.component';
 import { RegisterRoutingModule } from './register-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  imports: [ThemeModule,RegisterRoutingModule],
+  imports: [
+      ThemeModule,
+      RegisterRoutingModule,
+      ReactiveFormsModule,
+      FormsModule
+  ],
   declarations: [RegisterComponent],
   providers: []
 })
